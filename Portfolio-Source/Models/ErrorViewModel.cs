413f81d1c3b42269fe265f7e7ba23f35ea7a9792
MHusna <mehmethusnak@gmail.com>
2023-05-18 14:34:39 +0300

    Proje oluşturuldu.

---
+++ Portfolio-Source/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio_Source.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
