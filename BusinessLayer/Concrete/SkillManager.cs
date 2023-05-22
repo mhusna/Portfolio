1e51eeea838428e5dbbb687c8ffc0b9b64c5602f
MHusna <mehmethusna@outlook.com>
2023-05-22 19:49:26 +0300

    SkillManager sınıfı yazıldı.

---
+++ BusinessLayer/Concrete/SkillManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SkillManager : IGenericService<Skill>
+    {
+        ISkillDal _skillDal;
+        public SkillManager(ISkillDal skillDal)
+        {
+            _skillDal = skillDal;
+        }
+
+        public void TAdd(Skill entity)
+        {
+            _skillDal.Insert(entity);
+        }
+
+        public void TDelete(Skill entity)
+        {
+            _skillDal.Delete(entity);   
+        }
+
+        public Skill TGetById(int id)
+        {
+            return _skillDal.GetById(id);
+        }
+
+        public List<Skill> TGetList()
+        {
+            return _skillDal.GetList();
+        }
+
+        public void TUpdate(Skill entity)
+        {
+            _skillDal.Update(entity);
+        }
+    }
+}
