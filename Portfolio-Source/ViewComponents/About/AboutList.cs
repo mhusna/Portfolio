bd41831fe65babf773b781fdb15319252957a4e7
MHusna <mehmethusna@outlook.com>
2023-05-22 19:18:12 +0300

    About viewcomponent yapısı oluşturuldu ve index.html içerisinde çağırıldı.

---
+++ Portfolio-Source/ViewComponents/About/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.About
+{
+    public class AboutList:ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(aboutManager.TGetList());
+        }
+    }
+}
