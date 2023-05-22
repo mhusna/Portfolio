99de72fed9c766b6f9560e75dd3f625178157ec4
MHusna <mehmethusna@outlook.com>
2023-05-22 20:08:55 +0300

    Skill varlığı viewcomponent yapıldı ve index.html içerisinde çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Skill
+{
+    public class SkillList:ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(skillManager.TGetList());
+        }
+    }
+}
