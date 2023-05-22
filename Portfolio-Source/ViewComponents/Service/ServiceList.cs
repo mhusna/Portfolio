38e1f295fd46d251f5969c3bd1275f63e2ea74eb
MHusna <mehmethusna@outlook.com>
2023-05-22 19:43:11 +0300

    Service alanı viewcomponent yapıldı ve index.html içerisinde çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Portfolio_Source.ViewComponents.Service
+{
+    public class ServiceList:ViewComponent
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(serviceManager.TGetList());
+        }
+    }
+}
