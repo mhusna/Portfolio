a4ff6aad9c27aa10d978e260f1384a4c5f64dbe0
MHusna <mehmethusna@outlook.com>
2023-05-23 12:37:01 +0300

    Portfolio varlığı migration işlemi yapıldı.

---
+++ DataAccessLayer/Migrations/20230523093525_portfolioEntityRevise.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class portfolioEntityRevise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Portfolios",
+                newName: "ProjectUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageFullUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageShortUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageFullUrl",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "ImageShortUrl",
+                table: "Portfolios");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectUrl",
+                table: "Portfolios",
+                newName: "ImageUrl");
+        }
+    }
+}
