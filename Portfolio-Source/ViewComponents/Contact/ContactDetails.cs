a26a9baeefbcb83b15fab5184cd6b3a9ed9837e7
MHusna <mehmethusna@outlook.com>
2023-05-23 15:49:31 +0300

    İletişim alanı viewcomponent yapıldı ve index.html içerisinden çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Contact
+{
+    public class ContactDetails:ViewComponent
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(contactManager.TGetList());
+        }
+    }
+}
