8b7b5a3bc147c8b039535a5cfe463544de826a87
MHusna <mehmethusna@outlook.com>
2023-05-23 15:05:49 +0300

    Testimonial varlığına Title alanı eklendi.

--- EntityLayer/Concrete/Testimonial.cs
+++ EntityLayer/Concrete/Testimonial.cs
@@ -12,6 +12,7 @@ namespace EntityLayer.Concrete
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Title { get; set; }
         public string Company { get; set; }
         public string Comment { get; set; }
         public string ImageUrl { get; set; }
