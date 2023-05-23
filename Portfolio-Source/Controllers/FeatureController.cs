788d414cbce1a5e949c78ab5a52e80314d073a86
MHusna <mehmethusna@outlook.com>
2023-05-23 19:24:39 +0300

    Admin dashboard yüklendi ve işlemlere başlandı.

---
+++ Portfolio-Source/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
