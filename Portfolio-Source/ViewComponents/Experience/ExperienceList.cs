b01833ed414e9c4198caed4588c8a43e85c767ba
MHusna <mehmethusna@outlook.com>
2023-05-23 14:46:40 +0300

    Experience bölümü viewcomponent yapıldı ve index.html içerisinde çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Experience
+{
+    public class ExperienceList:ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(experienceManager.TGetList());
+        }
+    }
+}
