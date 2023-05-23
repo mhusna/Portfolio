e230cd750bd36f2a8eaa1e3704cd6919022667a3
MHusna <mehmethusna@outlook.com>
2023-05-23 15:15:20 +0300

    Testimonial alanı viewcomponent yapıldı ve index.html içerisinden çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Testimonial/TestimonialList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Testimonial
+{
+    public class TestimonialList:ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View(testimonialManager.TGetList());
+        }
+    }
+}
