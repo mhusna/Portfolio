1341fd1fdf52de1dbfa36e436ad3e861ff6a44f6
MHusna <mehmethusna@outlook.com>
2023-05-23 12:55:31 +0300

    Portfolio alanı viewcomponent yapıldı ve index.html içerisinden çağırıldı.

---
+++ Portfolio-Source/ViewComponents/PortfolioList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents
+{
+    public class PortfolioList:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(portfolioManager.TGetList());
+        }
+    }
+}
