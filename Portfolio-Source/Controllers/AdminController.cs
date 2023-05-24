e068f5ed7edb77f32355e64ee4f48ea6581c8dcc
MHusna <mehmethusna@outlook.com>
2023-05-24 15:54:43 +0300

    Admin controller içerisine partial view metotları yazıldı.

--- Portfolio-Source/Controllers/AdminController.cs
+++ Portfolio-Source/Controllers/AdminController.cs
@@ -4,9 +4,30 @@ namespace Portfolio_Source.Controllers
 {
     public class AdminController : Controller
     {
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
         public PartialViewResult PartialSideBar()
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        
     }
 }
