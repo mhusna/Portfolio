0dc47cfac4ecb40e5afe7398434f7c34077a8493
MHusna <mehmethusna@outlook.com>
2023-05-26 12:31:07 +0300

    ExperienceController sınıfı oluşturuldu ve index metodu yazıldı.

---
+++ Portfolio-Source/Controllers/ExperienceController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
+        public IActionResult Index()
+        {
+            ViewBag.Message = "## Experience List ##";
+            ViewBag.Controller = "Experience";
+            ViewBag.Action = "Index";
+            return View(experienceManager.TGetList());
+        }
+    }
+}
