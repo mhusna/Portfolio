3860bcc46ee621a4427e90b2f1cf697c9d816341
MHusna <mehmethusna@outlook.com>
2023-05-29 14:21:39 +0300

    ServiceController yazıldı.

---
+++ Portfolio-Source/Controllers/ServiceController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceRepository());
+        public IActionResult Index()
+        {
+            return View(serviceManager.TGetList());
+        }
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            return View(serviceManager.TGetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            serviceManager.TDelete(serviceManager.TGetById(id));
+            return RedirectToAction("Index");
+        }
+    }
+}
