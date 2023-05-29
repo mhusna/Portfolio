28ae361ddd000b8fc9ada86f44d06542e8bb443e
MHusna <mehmethusna@outlook.com>
2023-05-29 13:40:47 +0300

    AboutController oluşturuldu.

---
+++ Portfolio-Source/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
