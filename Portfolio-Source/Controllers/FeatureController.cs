ea70d398594c4957dd8e76678fb5d9cdaed44395
MHusna <mehmethusna@outlook.com>
2023-05-29 13:09:11 +0300

    FeatureController Index metodu yazıldı.

--- Portfolio-Source/Controllers/FeatureController.cs
+++ Portfolio-Source/Controllers/FeatureController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Source.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureRepository());
         public IActionResult Index()
         {
-            return View();
+            return View(featureManager.TGetList());
         }
     }
 }
