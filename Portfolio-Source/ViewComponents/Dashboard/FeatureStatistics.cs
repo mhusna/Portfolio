0cbff7ef01cb5395d9cea8e35574d1052b4ccaf6
MHusna <mehmethusna@outlook.com>
2023-05-30 15:51:39 +0300

    Dashboard için Feature alanı viewcomponent yapıldı.

---
+++ Portfolio-Source/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class FeatureStatistics:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
