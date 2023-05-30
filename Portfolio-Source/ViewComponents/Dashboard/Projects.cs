135bdf04c605b60d4961a707eb61f562b352ab8d
MHusna <mehmethusna@outlook.com>
2023-05-30 16:47:21 +0300

    Projelerin listelenme işlemi viewcomponent yapıldı.

---
+++ Portfolio-Source/ViewComponents/Dashboard/Projects.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class Projects:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(portfolioManager.TGetList().GetRange(portfolioManager.TGetList().Count() - 5, 5));
+        }
+    }
+}
