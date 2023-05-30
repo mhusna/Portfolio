7886133e5146e04ab8c6835b02bfb42e9cdd2c11
MHusna <mehmethusna@outlook.com>
2023-05-30 16:20:39 +0300

    FeatureStatistics alanları düzenlendi.

--- Portfolio-Source/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Portfolio-Source/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Source.ViewComponents.Dashboard
 {
     public class FeatureStatistics:ViewComponent
     {
+        SkillManager skillManager = new SkillManager(new EfSkillRepository());
+        MessageManager messageManager = new MessageManager(new EfMessageRepository());
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
         public IViewComponentResult Invoke()
         {
+            ViewBag.SkillCount = skillManager.TGetList().Count();
+            ViewBag.ExperienceCount = experienceManager.TGetList().Count();
+            ViewBag.UnreadMessages = messageManager.TGetList().Where(message => message.Status == false).Count();
+            ViewBag.ReadMessages = messageManager.TGetList().Where(message => message.Status == true).Count();
+
             return View();
         }
     }
