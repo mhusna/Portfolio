a694c9688a16cbf17044d69b93ad309c1988bab2
MHusna <mehmethusna@outlook.com>
2023-06-01 15:13:18 +0300

    MainStatistics viewcomponent olarak eklendi.

---
+++ Portfolio-Source/ViewComponents/Dashboard/MainStatistics.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class MainStatistics:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioRepository());
+        ServiceManager serviceManager = new ServiceManager(new EfServiceRepository());
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Services = serviceManager.TGetList().Count;
+            ViewBag.Portfolios = portfolioManager.TGetList().Count;
+            ViewBag.Experiences = experienceManager.TGetList().Count;
+            return View();
+        }
+    }
+}
