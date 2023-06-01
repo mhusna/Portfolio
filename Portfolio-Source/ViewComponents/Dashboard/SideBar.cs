7a17d3732b59628fbdce52ad46a76f66c6ef29cd
MHusna <mehmethusna@outlook.com>
2023-06-01 13:40:46 +0300

    Dashboard içerisindeki sidebar viewcomponent yapısına çevirildi, menüler tasarlandı ve dashboard içerisinden çağırıldı.

---
+++ Portfolio-Source/ViewComponents/Dashboard/SideBar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class SideBar:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
