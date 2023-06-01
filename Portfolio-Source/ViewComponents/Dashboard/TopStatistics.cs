c534bc9d379863dd189293d1a0e10d32eb79e5cc
MHusna <mehmethusna@outlook.com>
2023-06-01 15:12:31 +0300

    FeatureStatistics revize edilerek TopStatistics kodlandı.

--- Portfolio-Source/ViewComponents/Dashboard/TopStatistics.cs
+++ Portfolio-Source/ViewComponents/Dashboard/TopStatistics.cs
@@ -4,15 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Source.ViewComponents.Dashboard
 {
-    public class FeatureStatistics:ViewComponent
+    public class TopStatistics:ViewComponent
     {
-        SkillManager skillManager = new SkillManager(new EfSkillRepository());
         MessageManager messageManager = new MessageManager(new EfMessageRepository());
-        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
         public IViewComponentResult Invoke()
         {
-            ViewBag.SkillCount = skillManager.TGetList().Count();
-            ViewBag.ExperienceCount = experienceManager.TGetList().Count();
             ViewBag.UnreadMessages = messageManager.TGetList().Where(message => message.Status == false).Count();
             ViewBag.ReadMessages = messageManager.TGetList().Where(message => message.Status == true).Count();
 
