83f384fc9e548c29b00d7bdf63f5caf062c01261
MHusna <mehmethusna@outlook.com>
2023-06-03 21:17:45 +0300

    Dashboard messagelist alani viewcomponent yapildi.

---
+++ Portfolio-Source/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class MessageList:ViewComponent
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(messageManager.TGetList());
+        }
+    }
+}
