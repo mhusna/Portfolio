0af6ca651fb1fda8ebd20c7b58ba3e7089c0d6aa
MHusna <mehmethusna@outlook.com>
2023-06-03 21:44:15 +0300

    User varligi icin DAL katmaninda interface ve class tanimlandi.

---
+++ DataAccessLayer/Concrete/Repository/EfRepository/EfUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository.EfRepository
+{
+    public class EfUserRepository : GenericRepository<User>, IUserDal
+    {
+    }
+}
