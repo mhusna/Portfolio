d4ce8db5ba7ff7b85d2f1970162ee306ba0418fd
MHusna <mehmethusna@outlook.com>
2023-06-03 21:50:33 +0300

    Business katmaninda User varligi ile ilgili islemler yapildi.

---
+++ BusinessLayer/Concrete/UserManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public void TAdd(User entity)
+        {
+            _userDal.Insert(entity);
+        }
+
+        public void TDelete(User entity)
+        {
+            _userDal.Delete(entity);
+        }
+
+        public User TGetById(int id)
+        {
+            return _userDal.GetById(id);
+        }
+
+        public List<User> TGetList()
+        {
+            return _userDal.GetList();
+        }
+
+        public void TUpdate(User entity)
+        {
+            _userDal.Update(entity);
+        }
+    }
+}
