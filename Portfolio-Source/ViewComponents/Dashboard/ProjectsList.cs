53129cdd15907ecbe607cf351075b8c40a04a4a0
MHusna <mehmethusna@outlook.com>
2023-06-03 14:38:46 +0300

    Proje listesi dashboard'a viewcomponent olarak dahil edildi.

---
+++ Portfolio-Source/ViewComponents/Dashboard/ProjectsList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class ProjectsList:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(portfolioManager.TGetList());
+        }
+    }
+}
