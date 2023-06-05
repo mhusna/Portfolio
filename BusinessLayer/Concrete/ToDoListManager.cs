60c7c58685a03f1f806a62f7e7d72d2177865f86
MHusna <mehmethusna@outlook.com>
2023-06-05 12:59:11 +0300

    ToDoList entity Business katmani yazildi.

---
+++ BusinessLayer/Concrete/ToDoListManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ToDoListManager : IToDoListService
+    {
+        ITodoListDal _toDoListDal;
+
+        public ToDoListManager(ITodoListDal todoListDal)
+        {
+            _toDoListDal = todoListDal;
+        }
+        public void TAdd(ToDoList entity)
+        {
+            _toDoListDal.Insert(entity);
+        }
+
+        public void TDelete(ToDoList entity)
+        {
+            _toDoListDal.Delete(entity);
+        }
+
+        public ToDoList TGetById(int id)
+        {
+            return _toDoListDal.GetById(id);
+        }
+
+        public List<ToDoList> TGetList()
+        {
+            return _toDoListDal.GetList();
+        }
+
+        public void TUpdate(ToDoList entity)
+        {
+            _toDoListDal.Update(entity);
+        }
+    }
+}
