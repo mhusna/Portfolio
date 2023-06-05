aad8d5c54c09e6d9ad865acfe58b50bf906556f4
MHusna <mehmethusna@outlook.com>
2023-06-05 13:16:33 +0300

    ToDoList viewcomponent hale getirildi ve index icerisinde cagirildi.

---
+++ Portfolio-Source/ViewComponents/Dashboard/ToDoLists.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio_Source.ViewComponents.Dashboard
+{
+    public class ToDoLists:ViewComponent
+    {
+        ToDoListManager manager = new ToDoListManager(new EfToDoListRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(manager.TGetList());
+        }
+    }
+}
