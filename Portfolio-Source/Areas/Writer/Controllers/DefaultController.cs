68f46adcda85c3e73dff5841659499f2e1ce2095
MHusna <mehmethusna@outlook.com>
2023-06-06 11:50:45 +0300

    Writer Area olusturuldu.

---
+++ Portfolio-Source/Areas/Writer/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Areas.Writer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Area("Writer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
