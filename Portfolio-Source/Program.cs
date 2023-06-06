89371242b4b4f2fd093b0f31c1c41f54110baffd
MHusna <mehmethusna@outlook.com>
2023-06-06 11:50:12 +0300

    Area program.cs import edildi.

--- Portfolio-Source/Program.cs
+++ Portfolio-Source/Program.cs
@@ -30,6 +30,14 @@ namespace Portfolio_Source
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
+                );
+            });
+
             app.Run();
         }
     }
