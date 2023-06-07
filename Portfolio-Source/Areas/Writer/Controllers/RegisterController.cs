0725637fa8c70e29c92059765b0bbbe94392a5f3
MHusna <mehmethusna@outlook.com>
2023-06-07 09:22:16 +0300

    RegisterController Index metodu yazildi.

--- Portfolio-Source/Areas/Writer/Controllers/RegisterController.cs
+++ Portfolio-Source/Areas/Writer/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Portfolio_Source.Areas.Writer.Models;
 
 namespace Portfolio_Source.Areas.Writer.Controllers
@@ -6,6 +9,12 @@ namespace Portfolio_Source.Areas.Writer.Controllers
     [Area("Writer")]
     public class RegisterController : Controller
     {
+        private readonly UserManager<WriterUser> _userManager;
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -13,11 +22,31 @@ namespace Portfolio_Source.Areas.Writer.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(UserRegisterViewModel model)
+        public async Task<IActionResult> Index(UserRegisterViewModel model)
         {
             if(ModelState.IsValid)
             {
+                WriterUser writerUser = new WriterUser()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Mail,
+                    ImageUrl = model.ImageUrl
+                };
 
+                var result = await _userManager.CreateAsync(writerUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach(var item in result.Errors) {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
             }
             return View();
         }
