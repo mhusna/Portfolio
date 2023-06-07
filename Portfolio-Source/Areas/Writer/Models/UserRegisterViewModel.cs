4729c788e58425c0e9c0ffbcee0fa3c042639087
MHusna <mehmethusna@outlook.com>
2023-06-07 08:34:57 +0300

    UserRegisterViewModel yazildi.

---
+++ Portfolio-Source/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio_Source.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Please enter the user name.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the password.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter the password again.")]
+        [Compare("Password", ErrorMessage ="Passwords are not same.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter the mail address.")]
+        public string Mail  { get; set; }
+
+    }
+}
