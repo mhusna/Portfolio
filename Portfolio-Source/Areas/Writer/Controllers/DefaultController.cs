8ebe3e283aabac5bd2aa723e3b594e37cbe6418b
MHusna <mehmethusna@outlook.com>
2023-06-09 11:30:06 +0300

    Default controller authorize annotation eklendi

--- Portfolio-Source/Areas/Writer/Controllers/DefaultController.cs
+++ Portfolio-Source/Areas/Writer/Controllers/DefaultController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Source.Areas.Writer.Controllers
 {
     public class DefaultController : Controller
     {
         [Area("Writer")]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
