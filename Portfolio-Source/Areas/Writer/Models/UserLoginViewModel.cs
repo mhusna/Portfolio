16bc81b44b2f51976b00a32d6c2cf4d74f924099
MHusna <mehmethusna@outlook.com>
2023-06-09 10:43:39 +0300

    Login sayfasi kodlari ve controlleri hazirlandi.

---
+++ Portfolio-Source/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio_Source.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "Enter the user name...")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Enter the password...")]
+        public string Password { get; set; }
+    }
+}
