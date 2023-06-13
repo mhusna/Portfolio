49833af9ae2659252dafb6f0b64118f55ba59af0
MHusna <mehmethusna@outlook.com>
2023-06-13 12:58:11 +0300

    AnnouncementDetail sayfası için controller metot eklendi.

--- Portfolio-Source/Areas/Writer/Controllers/DefaultController.cs
+++ Portfolio-Source/Areas/Writer/Controllers/DefaultController.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Source.Areas.Writer.Controllers
 {
+    [Area("Writer")]
     public class DefaultController : Controller
     {
-        [Area("Writer")]
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementRepository());
+
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            return View(announcementManager.TGetList());
         }
+
+        public IActionResult AnnouncementDetails(int id)
+        {
+            return View(announcementManager.TGetById(id));
+        }
+
     }
 }
