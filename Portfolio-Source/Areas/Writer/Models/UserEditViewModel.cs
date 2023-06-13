95a180e87fa29c5ee370a1c03699c046cfe45f57
MHusna <mehmethusna@outlook.com>
2023-06-13 16:06:05 +0300

    UserEditViewModel modeli oluşturuldu.

---
+++ Portfolio-Source/Areas/Writer/Models/UserEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio_Source.Areas.Writer.Models
+{
+    public class UserEditViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
