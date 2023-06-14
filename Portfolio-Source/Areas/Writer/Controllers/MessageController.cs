65a06b26311c0fd090fac73ab32cf607772fcbfe
MHusna <mehmethusna@outlook.com>
2023-06-14 13:25:10 +0300

    Writer sidebar ayarlaması.

---
+++ Portfolio-Source/Areas/Writer/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
