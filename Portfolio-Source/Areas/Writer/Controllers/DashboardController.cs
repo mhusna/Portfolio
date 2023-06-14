db8f4ec2f1a5f2c5c59d6441ed4785556a9b520a
MHusna <mehmethusna@outlook.com>
2023-06-14 12:53:58 +0300

    OpenweatherAPI'den hava durumu çekilmesi ve dashboard içerisinde gösterilmesi.

--- Portfolio-Source/Areas/Writer/Controllers/DashboardController.cs
+++ Portfolio-Source/Areas/Writer/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete.Repository.EfRepository;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Portfolio_Source.Areas.Writer.Controllers
 {
@@ -21,6 +22,14 @@ namespace Portfolio_Source.Areas.Writer.Controllers
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.FullName = user.FirstName + " "+ user.LastName;
 
+            //Weather API
+            string key = "8a66d901eee5089c9df93bdc7f0f1d45";
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+key;
+
+            XDocument document = XDocument.Load(url);
+            ViewBag.Temperature = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //Statistics 
             AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementRepository());
             ViewBag.Announcements = announcementManager.TGetList().Count;
             ViewBag.Messages = 0;
