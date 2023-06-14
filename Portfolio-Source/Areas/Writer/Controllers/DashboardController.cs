dcfe27d3f29effbb74655121dcd3694b3adfa726
MHusna <mehmethusna@outlook.com>
2023-06-14 12:35:11 +0300

    Writer için dashboard ayarlaması.

---
+++ Portfolio-Source/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repository.EfRepository;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.FullName = user.FirstName + " "+ user.LastName;
+
+            AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementRepository());
+            ViewBag.Announcements = announcementManager.TGetList().Count;
+            ViewBag.Messages = 0;
+            return View();
+        }
+    }
+}
