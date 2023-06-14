25712b7df9f5609a03b2d9cf9cc504672fdc8ada
MHusna <mehmethusna@outlook.com>
2023-06-14 16:29:49 +0300

    Announcement varlığı Status özelliği için veri tipi değiştirildi.

---
+++ DataAccessLayer/Migrations/20230614132747_announcementStatusChange.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class announcementStatusChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Announcements",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Announcements",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
