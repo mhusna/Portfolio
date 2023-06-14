9d3e7bb7f74419b03028b55d0f80ddb14ed61f8b
MHusna <mehmethusna@outlook.com>
2023-06-14 16:11:43 +0300

    _NavbarPartial içerisindeki Notification alanı viewcomponente çevirildi.

---
+++ Portfolio-Source/Areas/Writer/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Areas.Writer.ViewComponents.Notification
+{
+    public class NotificationList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
