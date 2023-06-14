1a5b15d962241300a8a61d827dddb512e3aaec35
MHusna <mehmethusna@outlook.com>
2023-06-14 17:32:29 +0300

    WriterUser kullanıcı resimi görüntüleme.

---
+++ Portfolio-Source/Areas/Writer/ViewComponents/Profile/Profile.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Source.Areas.Writer.ViewComponents.Profile
+{
+    public class Profile:ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public Profile(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.ImageUrl = user.ImageUrl;
+
+            return View();
+        }
+    }
+}
