27ab380699e4b436d8248c4b4a93e76bd0981029
MHusna <mehmethusna@outlook.com>
2023-06-15 20:44:23 +0300

    WriterMessage için filtreye göre liste döndüren metot eklendi.

--- DataAccessLayer/Concrete/Repository/EfRepository/EfWriterMessageRepository.cs
+++ DataAccessLayer/Concrete/Repository/EfRepository/EfWriterMessageRepository.cs
@@ -3,12 +3,20 @@ using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repository.EfRepository
 {
-    public class EfWriterMessageRepository:GenericRepository<WriterMessage>,IWriterMessageDal
+    public class EfWriterMessageRepository : GenericRepository<WriterMessage>, IWriterMessageDal
     {
+        public List<WriterMessage> GetByFilter(Expression<Func<WriterMessage, bool>> filter)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<WriterMessage>().Where(filter).ToList();
+            }
+        }
     }
 }
